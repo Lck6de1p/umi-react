7f3995f7cd8194ff2ab8e5dcc0395d3240e65f71
Lck6de1p <310504719@qq.com>
2021-07-09 15:34:10 +0800

    编写弹窗组件

--- src/pages/class/index.js
+++ src/pages/class/index.js
@@ -28,6 +28,9 @@ export default class Index extends Component {
           <List.Item>
             <Link to="/class/lazy-load">lazy-load</Link>
           </List.Item>
+          <List.Item>
+            <Link to="/class/modal">modal</Link>
+          </List.Item>
         </List>
 
       </div>
